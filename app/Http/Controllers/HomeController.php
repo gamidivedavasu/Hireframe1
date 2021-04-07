8410ecf19e5db3b62ed53e3c19d3a49a7ae35904
anthonyfernandes07 <77669497+anthonyfernandes07@users.noreply.github.com>
2021-04-07 16:50:27 +0100

    corrections

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('pages.index');
+        return view('index');
     }
 
     function about(){
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\FeedbackController;
 use App\Http\Controllers\TemplateController;
 use App\Http\Controllers\DashboardController;
