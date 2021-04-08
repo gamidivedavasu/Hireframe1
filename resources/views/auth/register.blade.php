c668019e605107ac8cfdee50f0c91b29304dd5a0
Vedavasu Gamidi <vedavasugamidi@gmail.com>
2021-04-08 23:24:00 +0100

    8th April

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="flex justify-center">
-        <div class="w-6/12 bg-white p-6 rounded-lg">
+        <div class="w-4/12 bg-white p-6 rounded-lg">
             <form action="{{ route('register') }}" method="post">
                 @csrf
                 <div class="mb-4">
