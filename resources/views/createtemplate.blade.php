c490c95b5d3ed9a6056bc5ab3eca7a56e56f636c
anthonyfernandes07 <77669497+anthonyfernandes07@users.noreply.github.com>
2021-04-08 11:42:08 +0100

    Update createtemplate.blade.php

--- resources/views/createtemplate.blade.php
+++ resources/views/createtemplate.blade.php
@@ -1,11 +1,5 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+@section('content')
     <form action="{{ route('createtemplate') }}" method="POST">
         @csrf
         Let's name your template:
@@ -36,5 +30,4 @@
         <br>
         <button type="submit"> Create template</button>
     </form>
-</body>
-</html>
+@endsection
