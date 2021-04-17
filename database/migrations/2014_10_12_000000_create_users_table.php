08584b8707a904d441c7721207423f5a440c11a9
Vedavasu Gamidi <vedavasugamidi@gmail.com>
2021-04-17 22:50:16 +0100

    17th

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('CV');
+            $table->file('CV');
             $table->rememberToken();
             $table->timestamps();
         });
